462246f9c72030898feecfbbc8efd5da36590855
unreal1984 <unreal1984@mail.ru>
2024-04-25 18:14:35 +0300

    DB added

--- mydjangoprogect/myapp1/models.py
+++ mydjangoprogect/myapp1/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Worker(models.Model):
+    name = models.CharField(max_length=20, blank=False)
+    second_name = models.CharField(max_length=35, blank=False)
+    salary = models.IntegerField(default=0)
