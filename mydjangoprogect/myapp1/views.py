dd09e99ab47cb78145274b498116a4d71a1cde72
unreal1984 <unreal1984@mail.ru>
2024-04-25 19:07:21 +0300

    all_workers added

--- mydjangoprogect/myapp1/views.py
+++ mydjangoprogect/myapp1/views.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
 from django.shortcuts import render
 from myapp1.models import Worker
 
@@ -5,6 +7,13 @@ from myapp1.models import Worker
 def index_page(request):
     all_workers = Worker.objects.all()
     print(all_workers)
+    
+    workers_filtered = Worker.objects.filter(salary=30000)
+    print(workers_filtered)
+    
+    for i in all_workers:
+        print(f"Surname: {i.second_name}, Name: {i.name}, Salary: {i.salary}, ID: {i.id}")
+                
     return render(request, 'index.html')
 #def about_page(request):
 #   return render(request, 'about.html')
