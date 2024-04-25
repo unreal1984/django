6c4e5b9f59d01b21cdc34794d9a664b9ff3ebb04
unreal1984 <unreal1984@mail.ru>
2024-04-25 18:21:14 +0300

    db admin show

--- mydjangoprogect/myapp1/models.py
+++ mydjangoprogect/myapp1/models.py
@@ -5,3 +5,6 @@ class Worker(models.Model):
     name = models.CharField(max_length=20, blank=False)
     second_name = models.CharField(max_length=35, blank=False)
     salary = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return f'{self.second_name} {self.name}'
