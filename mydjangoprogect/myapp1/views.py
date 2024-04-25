ccd2f1e22c31a61b2acc0756e6c89a695e0442d7
unreal1984 <unreal1984@mail.ru>
2024-04-25 18:30:41 +0300

    all workers add

--- mydjangoprogect/myapp1/views.py
+++ mydjangoprogect/myapp1/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from myapp1.models import Worker
 
 # Create your views here.
 def index_page(request):
+    all_workers = Worker.objects.all()
+    print(all_workers)
     return render(request, 'index.html')
 #def about_page(request):
 #   return render(request, 'about.html')
